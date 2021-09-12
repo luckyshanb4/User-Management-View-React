e76fea7d02802dea29cab00af6ca1303839ddc3e
luckyshanb4 <luckyshanb4@gmail.com>
2021-09-12 20:38:18 +0530

    add css styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Profile(props) {
     return (
-        <div>
-            <img src={props.imagePath} />
-            <h3>{props.name}</h3>
-            <h3>{props.dateCreated}</h3>
-            <h3>{props.role}</h3>
-            <h3>{props.status}</h3>    
+        <div className='profile' >
+            <img className='photo' src={props.imagePath} />
+            <h3 className='name' >{props.name}</h3>
+            <h3 className='dateCreated'>{props.dateCreated}</h3>
+            <h3 className='role'>{props.role}</h3>
+            <h3 className='status'>{props.status}</h3>    
             
         </div>
     );
