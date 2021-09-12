032b69d9956a8be75a42ed4d048524dae8d15490
luckyshanb4 <luckyshanb4@gmail.com>
2021-09-12 21:24:59 +0530

    css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 function Profile(props) {
     return (
         <div className='profile' >
-            <img className='photo' src={props.imagePath} />
-            <h3 className='name' >{props.name}</h3>
-            <h3 className='dateCreated'>{props.dateCreated}</h3>
-            <h3 className='role'>{props.role}</h3>
-            <h3 className='status'>{props.status}</h3>    
-            
+            <div className='photo'>
+                <img  src={props.imagePath} />
+            </div>
+            <div className='name'>
+                <h3  >{props.name}</h3> 
+            </div>
+            <div className='dateCreated'>
+                <h3 >{props.dateCreated}</h3>
+            </div>
+            <div className='role'>
+                <h3 >{props.role}</h3>
+            </div>
+            <div className='status'>
+                <h3 >{props.status}</h3> 
+            </div>
+       
         </div>
     );
 }
